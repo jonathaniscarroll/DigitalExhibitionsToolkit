bf6e376613cb9468e08ab51cfe8c8d2d65a8e592
Jonathan Edward Carroll <jonathaniscarroll@gmail.com>
2022-11-29 15:59:02 -0400

    working on the ui editor, starting with ability to create new pages and new blocks within pages

--- StringDictionaryList.cs
+++ StringDictionaryList.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 [CreateAssetMenu]
 public class StringDictionaryList : ScriptableObject
 {
@@ -8,7 +9,30 @@ public class StringDictionaryList : ScriptableObject
 	public class NamedDictionary{
 		public string name;
 		public NamedStringReferenceList dictionary;
+		public NamedDictionary(string _name,string _dictionaryName){
+			NamedStringReferenceList newDictionary = ScriptableObject.CreateInstance<NamedStringReferenceList>();
+			string path = "Assets/DigitalExhibitionsToolkit/ScriptableObjects/_UI/UI-Pages-"+_name+".asset";
+			AssetDatabase.CreateAsset(newDictionary,path);
+			EditorUtility.FocusProjectWindow();
+			Selection.activeObject = newDictionary;
+			name = _name;
+			dictionary = newDictionary;
+		}
 	}
 	
 	public List<NamedDictionary> DictionaryList;
+	public StringListEvent OutputNamesEvent;
+	
+	public void CreateNewEntry(string name, string scriptableObjectName){
+		NamedDictionary newDictionary = new NamedDictionary(name, scriptableObjectName);
+		DictionaryList.Add(newDictionary);
+	}
+	
+	public void OutputListOfNames(){
+		List<string> list = new List<string>();
+		foreach(NamedDictionary dict in DictionaryList){
+			list.Add(dict.name);
+		}
+		OutputNamesEvent.Invoke(list);
+	}
 }
